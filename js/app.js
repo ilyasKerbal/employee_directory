39619d4bfce1e02c88ad1dd0842eee6ee50a547a
ILYAS KERBAL <kerbalsc@gmail.com>
2020-04-08 10:39:41 +0100

    Project styling and structure

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    const randomUsersUrl = "https://randomuser.me/api/?results=18";
+
+    /*
+    * Fetch part
+    * */
+
+    /*
+    * Callback
+    * */
+
+    const insertEmployee = function(data){
+
+    }
+});
