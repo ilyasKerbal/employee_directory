0fc16e1a9b12ecd45f9dea65a158cd778bc3b128
ILYAS KERBAL <kerbalsc@gmail.com>
2020-04-08 12:01:40 +0100

    Fetching employees from API

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
-    const randomUsersUrl = "https://randomuser.me/api/?results=18";
-
-    /*
-    * Fetch part
-    * */
+    const randomUsersUrl = "https://randomuser.me/api/?results=18&nat=us";
+    const employeesList = document.querySelector('.employees-list');
+    const statusParagraph = document.getElementById('status');
 
     /*
     * Callback
     * */
 
-    const insertEmployee = function(data){
-
+    const insertEmployees = function(data) {
+        data.results.forEach((item) => {
+            employeesList.innerHTML += generateEmployee(item);
+        });
+        statusParagraph.style.display = "none";
     }
+
+    const generateEmployee = function(data){
+        return `
+            <div class="employee-item">
+            <img src="${data.picture.large}" alt="${data.name.first}' profile picture">
+                <div class="employee-info">
+                    <h3>${data.name.first} ${data.name.last}</h3>
+                    <p>${data.email}</p>
+                    <p>${data.location.city}</p>
+                </div>
+            </div>
+        `;
+    }
+
+    /*
+    * Fetch part
+    * */
+
+    fetch(randomUsersUrl).then(res => res.json()).then(insertEmployees);
 });
